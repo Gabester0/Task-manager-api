fc80946294374c3f724ffcf0c9c74943c51ce0bd
Gabester0 <gceipper@gmail.com>
2020-11-06 01:25:01 -0500

    explored basic testing functionality with acode example and adding tests

---
+++ src/math.js
@@ -0,0 +1,4 @@
+const calculate = (total, tipPercent = 0.25)=>(total * tipPercent) + total;
+module.exports = {
+    calculate
+}
--- tests/math.test.js
+++ tests/math.test.js
@@ -1,8 +1,13 @@
-test('hello world', ()=>{
+const {calculate } = require('../src/math')
 
+
+test('Should calculate total with tip', ()=>{
+    const total = calculate(10, .3)
+    expect(total).toBe(13)
+    // if(total !== 13) throw new Error('total tip should be 13.  Got', total)
 })
 
-
-test('This should fail', ()=>{
-    throw new Error('Fail')
+test('Should calculate total with default tip', ()=>{
+    const total = calculate(10)
+    expect(total).toBe(12.5)
 })
