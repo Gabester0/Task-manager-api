ecfb9cc2a974e304bedcb98ba672810856462795
Gabester0 <gceipper@gmail.com>
2020-11-07 01:59:10 -0500

    wrote tests to test 2 new functions

--- src/math.js
+++ src/math.js
@@ -1,4 +1,15 @@
 const calculate = (total, tipPercent = 0.25)=>(total * tipPercent) + total;
+
+const fahrenheitToCelsius = (temp) => {
+    return (temp - 32) / 1.8
+}
+
+const celsiusToFahrenheit = (temp) => {
+    return (temp * 1.8) + 32
+}
+
 module.exports = {
-    calculate
+    calculate,
+    fahrenheitToCelsius,
+    celsiusToFahrenheit
 }
--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,4 @@
-const {calculate } = require('../src/math')
+const {calculate, celsiusToFahrenheit, fahrenheitToCelsius } = require('../src/math')
 
 
 test('Should calculate total with tip', ()=>{
@@ -11,3 +11,21 @@ test('Should calculate total with default tip', ()=>{
     const total = calculate(10)
     expect(total).toBe(12.5)
 })
+
+test(`Should convert 32 F to 0 C`, ()=>{
+    const celsius = fahrenheitToCelsius(32);
+    expect(celsius).toBe(0)
+})
+
+test(`Should convert 0 C to 32 F`, ()=>{
+    const fahrenheit = celsiusToFahrenheit(0);
+    expect(fahrenheit).toBe(32)
+})
+
+//
+// Goal: Test temperature conversion functions
+//
+// 1. Export both functions and load them into test suite
+// 2. Create "Should convert 32 F to 0 C"
+// 3. Create "Should convert 0 C to 32 F"
+// 4. Run the Jest to test your work!
