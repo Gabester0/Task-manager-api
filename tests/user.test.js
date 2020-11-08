513c91a1f16fe8ffaec31be2c3d31f57579ff34e
Gabester0 <gceipper@gmail.com>
2020-11-08 15:29:48 -0500

    created tests to test successful and failed login attempts

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/models/user');
+
+const userOne = {
+    name: "Mike",
+    email: "mike@example.com",
+    password: "Wh@tttt!9"
+}
+
+beforeEach( async ()=>{
+    await User.deleteMany()
+    await new User(userOne).save()
+})
 
 test('Should signup a new user', async()=>{
     await request(app).post('/users').send({
@@ -7,4 +19,18 @@ test('Should signup a new user', async()=>{
         email: 'jesterGabe@test.com',
         password: 'MyPass777!@'
     }).expect(201)
+})
+
+test('Should login existing user', async()=>{
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('Login should fail if nonexisting user attempts login', async()=>{
+    await request(app).post('/users/login').send({
+        email: 'fake@fake.com',
+        password: 'fakkkerr4#'
+    }).expect(400)
 })
