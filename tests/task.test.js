9bb716051dcd9efaa4c984569f8fe58e319d45b1
Gabester0 <gceipper@gmail.com>
2020-11-13 19:52:38 -0500

    fixed issues with task tests and organized tests file

--- tests/task.test.js
+++ tests/task.test.js
@@ -28,6 +28,43 @@ test(`Should create task for user`, async ()=>{
     expect(task.completed).toEqual(false)
 })
 
+// Should not create task with invalid description/completed
+test(`Should not be able to create task with invalid values`, async ()=>{
+    // No Description field
+    await request(app)
+        .post(`/tasks`)
+        .set(`Authorization`, `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            completed: false
+        })
+        .expect(400);
+})
+
+// Empty Description Field
+test(`Should not be able to create task with empty description`, async ()=>{
+    await request(app)
+        .post(`/tasks`)
+        .set(`Authorization`, `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            description: ``,
+            completed: true
+        })
+        .expect(400);
+})
+
+// Completed is not a boolean
+test(`Should not be able to create task with non-boolean completed`, async ()=>{
+    await request(app)
+        .post(`/tasks`)
+        .set(`Authorization`, `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            description: 'Task Test',
+            completed: `later`
+        })
+        .expect(400);
+});
+
+
 test(`Should get only specific users tasks`, async ()=>{
     const response = await request(app)
         .get('/tasks')
@@ -37,16 +74,26 @@ test(`Should get only specific users tasks`, async ()=>{
     expect(response.body.length).toEqual(2)
 })
 
-test(`User should not be able to delete another users tasks`, async ()=>{
-    const response = await request(app)
-        .delete(`/tasks/${taskOne._id}`)
-        .set(`Authorization`, `Bearer ${userTwo.tokens[0].token}`)
-        .send()
-        .expect(400)
-    const task = await Task.findById(taskOne._id);
-    expect(task).not.toBeNull();
-})
 
+// Should fetch user task by id
+// Should not fetch user task by id if unauthenticated
+// Should not fetch other users task by id
+// Should fetch only completed tasks
+// Should fetch only incomplete tasks
+// Should sort tasks by description/completed/createdAt/updatedAt
+// Should fetch page of tasks
+
+
+// Should not update other users task
+test(`Shouldn't update a different users tasks`, async()=>{
+    const response = await request(app)
+        .patch(`/tasks/${taskThree._id}`)
+        .set(`Authorization`, `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            description: `I am task number three`
+        })
+        .expect(404)
+})
 
 // Should not update task with invalid description/completed
 test(`Should not update task with invalid values`, async()=>{
@@ -60,43 +107,23 @@ test(`Should not update task with invalid values`, async()=>{
 })
 
 
+test(`User should not be able to delete another users tasks`, async ()=>{
+    const response = await request(app)
+        .delete(`/tasks/${taskOne._id}`)
+        .set(`Authorization`, `Bearer ${userTwo.tokens[0].token}`)
+        .send()
+        .expect(400)
+    const task = await Task.findById(taskOne._id);
+    expect(task).not.toBeNull();
+})
 
-//  
+
 // Should not delete task if unauthenticated
-// Should not update other users task
-// Should fetch user task by id
-// Should not fetch user task by id if unauthenticated
-// Should not fetch other users task by id
-// Should fetch only completed tasks
-// Should fetch only incomplete tasks
-// Should sort tasks by description/completed/createdAt/updatedAt
-// Should fetch page of tasks
-
-
-// Should not create task with invalid description/completed
-test(`Should not be able to create task with invalid values`, async ()=>{
-    // No Description field
-    const noDescription = taskOne
-    delete noDescription.description
+test(`Should not delete tasks if not logged in`, async()=>{
     await request(app)
-        .post(`/tasks`)
-        .set(`Authorization`, `Bearer ${userOne.tokens[0].token}`)
-        .send({ ...noDescription})
-        .expect(400);
-    // Empty Description Field
-    const emptyDescription = taskOne;
-    emptyDescription.description = ``;
-    await request(app)
-        .post(`/tasks`)
-        .set(`Authorization`, `Bearer ${userOne.tokens[0].token}`)
-        .send({ ...emptyDescription})
-        .expect(400);
-    // Completed is not a boolean
-    const invalidCompleted = taskOne;
-    invalidCompleted.completed = `false`
-    await request(app)
-        .post(`/tasks`)
-        .set(`Authorization`, `Bearer ${userOne.tokens[0].token}`)
-        .send({ ...invalidCompleted})
-        .expect(400);
-});
+        .delete(`/tasks/${taskThree._id}`)
+        .send()
+        .expect(401)
+    const task = Task.findById(taskThree._id);
+    expect(task).not.toBeNull();
+})
